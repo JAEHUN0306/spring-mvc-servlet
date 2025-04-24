2f8d86bfdbc0ec8b9c9aaa406a3b7f72281ead55
jaehun306 <jaehun306@naver.com>
2025-04-24 19:08:42 +0900

    서블릿으로 웹 애플리케이션 만들기

---
+++ src/main/java/hello/servlet/basic/domain/MemberRepository.java
@@ -0,0 +1,42 @@
+package hello.servlet.basic.domain;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MemberRepository {
+
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequance = 0;
+
+    private static final MemberRepository instance = new MemberRepository();
+
+    public static MemberRepository getInstance() {
+        return instance;
+    }
+
+    private MemberRepository() {
+    }
+
+    public Member save(Member member) {
+        member.setId(++sequance);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    public Member findById(Long id) {
+        return store.get(id);
+    }
+
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public void clearStore() {
+        store.clear();
+    }
+}
+
